05e6262f53e918ed0fde2e418903f5e980a291a6
shivam8188 <shivamkumar818894@gmail.com>
2026-02-11 20:52:58 +0530

    Update API URL construction for like and save functions

--- frontend/src/pages/general/Home.jsx
+++ frontend/src/pages/general/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
 
     async function likeVideo(item) {
 
-        const response = await axios.post("http://localhost:3000/api/food/like", { foodId: item._id }, {withCredentials: true})
+        const response = await axios.post(url + "/api/food/like", { foodId: item._id }, {withCredentials: true})
 
         if(response.data.like){
             console.log("Video liked");
@@ -35,7 +35,7 @@ const Home = () => {
     }
 
     async function saveVideo(item) {
-        const response = await axios.post("http://localhost:3000/api/food/save", { foodId: item._id }, { withCredentials: true })
+        const response = await axios.post( url + "/api/food/save", { foodId: item._id }, { withCredentials: true })
         
         if(response.data.save){
             setVideos((prev) => prev.map((v) => v._id === item._id ? { ...v, savesCount: v.savesCount + 1 } : v))
