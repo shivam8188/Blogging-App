42da6f5840c0d2690dd9321eda55859a6e2a407d
Shivam Kumar Vishwakarma <shivamkumar818894@gmail.com>
2026-06-18 09:57:13 +0530

    Add useNavigate import to Home component

--- frontend/src/pages/general/Home.jsx
+++ frontend/src/pages/general/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import '../../styles/reels.css'
 import ReelFeed from '../../components/ReelFeed'
 
